78fc766bdba0c0275001826f9891b8dad9678b1a
AniketRatan27 <ratan.aniket09@gmail.com>
2023-11-29 00:03:02 +0530

    CleanUp the game carts

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,7 +19,7 @@ const GameGrid = ({ gameQuery }: Props) => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 3 }}
         padding="10px"
-        spacing={4}
+        spacing={6}
       >
         {isLoading &&
           Skeleton.map((skeleton) => (
